07895926ff6687244d19ac777a25bd56ceb6711e
Atif Afzal <atif5801@gmail.com>
2019-05-15 13:59:10 +0530

    init

---
+++ fuse.ts
@@ -0,0 +1,41 @@
+import { FuseBox } from 'fuse-box';
+import { src, task } from 'fuse-box/sparky';
+
+const fuse = FuseBox.init({
+  homeDir: '.',
+  target: 'browser@es5',
+  output: 'dist/$name.bundle.js',
+});
+
+task('copy-assets', () => {
+  return src('**/**.**', { base: './#/assets' })
+    .clean('./dist')
+    .dest('./dist')
+    .exec();
+});
+
+task('build', () => {
+  fuse
+    .bundle('background')
+    .instructions('> #/background/index.ts')
+    .watch();
+
+  fuse
+    .bundle('content')
+    .instructions('> #/content/index.ts')
+    .watch();
+
+  fuse
+    .bundle('devtools')
+    .instructions('> #/devtools/index.ts')
+    .watch();
+
+  fuse
+    .bundle('popup')
+    .instructions('> #/popup/index.ts')
+    .watch();
+
+  return fuse.run();
+});
+
+task('default', ['copy-assets', 'build']);
