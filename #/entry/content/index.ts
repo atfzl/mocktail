f5eed54019238259c8dd771ff62427682157c35f
Atif Afzal <atif5801@gmail.com>
2019-05-17 16:51:52 +0530

    send events from inject to content script

--- #/entry/content/index.ts
+++ #/entry/content/index.ts
@@ -4,20 +4,51 @@ import { Store } from 'webext-redux';
 // tslint:disable-next-line:no-console
 console.log('content');
 
-const injectScript = (script: string) => {
-  const s = document.createElement('script');
+{
+  const injectScript = (script: string) => {
+    const s = document.createElement('script');
 
-  const src = chrome.extension.getURL(script);
+    const src = chrome.extension.getURL(script);
 
-  s.src = src;
-  s.onload = () => {
-    s.remove();
+    s.src = src;
+    s.onload = () => {
+      s.remove();
+    };
+
+    document.documentElement.appendChild(s);
   };
 
-  document.documentElement.appendChild(s);
-};
+  injectScript('inject.bundle.js');
+}
 
-injectScript('inject.bundle.js');
+interface Data<T = any> {
+  body: T;
+  evt: string;
+  src: string;
+}
+
+window.addEventListener('message', event => {
+  if (event.source !== window) {
+    return;
+  }
+
+  const data: Data = event.data;
+
+  if (!data || data.src !== 'xhook-atfzl') {
+    return;
+  }
+
+  switch (data.evt) {
+    case 'enable': {
+      console.log('hook enabled');
+      break;
+    }
+    case 'before': {
+      console.log('hook before', data.body);
+      break;
+    }
+  }
+});
 
 function main() {
   const store = new Store();
--- #/entry/inject/index.ts
+++ #/entry/inject/index.ts
@@ -1,33 +1,10 @@
 import { xhook } from 'xhook';
 
+const src = 'xhook-atfzl';
+
 xhook.enable();
+window.postMessage({ src, evt: 'enable' }, '*');
 
-xhook.before((request: any, cb: any) => {
-  if (
-    request.url.includes(
-      '/popularsearch?channel=web&child_site_id=1&site_id=1&version=2&cat_tree=1',
-    )
-  ) {
-    const data = {
-      app_display_count: 15,
-      keywords: [
-        { text: 'foobarasdjasd' },
-        { text: 'Mobile Recharge' },
-        { text: 'Electricity Payment' },
-        { text: 'Movie Tickets' },
-        { text: 'Bus Ticket' },
-        { text: 'Travel Dhamaka Sale' },
-        { text: 'Buy Digital Gold' },
-        { text: "Spencer's" },
-        { text: 'Bazaar' },
-        { text: 'Samsung Galaxy S10' },
-      ],
-    };
-
-    cb({ data, status: 200, text: JSON.stringify(data) });
-
-    window.postMessage({ source: 'atfzl', data: 'modified api call' }, '*');
-  } else {
-    cb();
-  }
+xhook.before((request: any) => {
+  window.postMessage({ src, evt: 'before', body: request.url }, '*');
 });
