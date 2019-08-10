9df81255d2b74ccde608d052b9373c788209f336
Harshit Thukral <h.thakral369@gmail.com>
2019-08-11 00:25:54 +0530

    chore: update config and add snapshots

--- #/entry/inject/__tests__/index.test.ts
+++ #/entry/inject/__tests__/index.test.ts
@@ -1,33 +1,66 @@
 import 'isomorphic-fetch';
 import '..';
 
+import nanoid = require('nanoid');
+
 const globalAny = global as any;
 
+let postMessage: jest.Mock<any, any>;
+
+// mock for nanoid to return same if for all
+jest.mock('nanoid');
+(nanoid as any).mockImplementation(() => 1);
+
 describe('Inject Script', () => {
-  const xhr = new XMLHttpRequest();
-  let postMessage: jest.Mock<any, any>;
+  const makeXHR = (...args: Parameters<XMLHttpRequest['open']>) => {
+    const xhr = new XMLHttpRequest();
+    return new Promise((resolve, reject) => {
+      xhr.open(...args);
+      xhr.send();
+      xhr.onload = resolve;
+      xhr.onerror = reject;
+    });
+  };
 
   beforeEach(() => {
     postMessage = globalAny.postMessage = jest.fn();
   });
 
   test('XMLHTTPRequests: postMessage called twice', done => {
-    xhr.open('GET', globalAny.getUrl('/json'));
-    xhr.send();
-    xhr.onload = () => {
-      expect(postMessage).toBeCalledTimes(2);
-      done();
-    };
-
-    xhr.onerror = err => {
-      console.error(err);
-    };
+    makeXHR('GET', globalAny.getUrl('/json'), true)
+      .then(() => {
+        expect(postMessage).toBeCalledTimes(2);
+        done();
+      })
+      .catch(console.error);
   });
 
   test('Fetch: postMessage called twice', done => {
-    fetch(globalAny.getUrl('/json')).then(() => {
-      expect(postMessage).toBeCalledTimes(2);
-      done();
-    });
+    fetch(globalAny.getUrl('/json'))
+      .then(() => {
+        expect(postMessage).toBeCalledTimes(2);
+        done();
+      })
+      .catch(console.error);
+  });
+
+  test('XMLHTTPRequests: Hook snapshot', done => {
+    makeXHR('GET', globalAny.getUrl('/json'), true)
+      .then(() => {
+        expect(postMessage.mock.calls[0]).toMatchSnapshot();
+        expect(postMessage.mock.calls[1]).toMatchSnapshot();
+        done();
+      })
+      .catch(console.error);
+  });
+
+  test('Fetch: Hook snapshot', done => {
+    fetch(globalAny.getUrl('/json'))
+      .then(() => {
+        expect(postMessage.mock.calls[0]).toMatchSnapshot();
+        expect(postMessage.mock.calls[1]).toMatchSnapshot();
+        done();
+      })
+      .catch(console.error);
   });
 });
